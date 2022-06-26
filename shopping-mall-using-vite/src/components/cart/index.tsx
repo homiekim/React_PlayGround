dd77a14dc00b16a026a307347cd5130504f5d5b6
HomieKim <ghals0708@gmail.com>
2022-06-26 23:46:08 +0900

    cart delete & update

--- shopping-mall-using-vite/src/components/cart/index.tsx
+++ shopping-mall-using-vite/src/components/cart/index.tsx
@@ -1,15 +1,48 @@
-
-import { CartType } from '../../graphql/cart'
-import CartItem from './item'
+import { SyntheticEvent, useCallback, useRef } from "react";
+import { CartType } from "../../graphql/cart";
+import CartItem from "./item";
 
 const CartList = ({ items }: { items: CartType[] }) => {
-  return (
-    <ul>
-      {items.map(item => (
-        <CartItem {...item} key={item.id} />
-      ))}
-    </ul>
-  )
-}
+  const formRef = useRef<HTMLFormElement>(null);
 
-export default CartList
+  const handleCheckboxChanged = useCallback((e: SyntheticEvent) => {
+    if (!formRef.current) return;
+    const checkboxes = formRef.current.querySelectorAll<HTMLInputElement>(
+      ".cart-item__checkbox"
+    );
+    const targetInput = e.target as HTMLInputElement;
+    const data = new FormData(formRef.current);
+    const selectedCount = data.getAll("select-item").length;
+
+    if(targetInput.classList.contains('select-all')){
+      // select-all 선택
+      const allChecked = targetInput.checked
+      checkboxes.forEach(inputElem => {
+        inputElem.checked = allChecked
+      })
+    }else{
+      // 개별아이템 선택시
+      const allChecked = selectedCount === items.length
+      formRef.current.querySelector<HTMLInputElement>('.select-all')!.checked = allChecked
+    
+    }
+  }, [formRef.current]);
+
+  return (
+    <div>
+      <form ref={formRef} onChange={handleCheckboxChanged}>
+        <label>
+          <input type="checkbox" name="select-all" className="select-all" />
+          전체선택
+        </label>
+        <ul className="cart">
+          {items.map((item) => (
+            <CartItem {...item} key={item.id} />
+          ))}
+        </ul>
+      </form>
+    </div>
+  );
+};
+
+export default CartList;
--- shopping-mall-using-vite/src/components/cart/item.tsx
+++ shopping-mall-using-vite/src/components/cart/item.tsx
@@ -1,10 +1,88 @@
+import { SyntheticEvent, useCallback } from "react";
+import { Query, useMutation, useQueryClient } from "react-query";
+import { CartType, DELETE_CART, UPDATE_CART } from "../../graphql/cart";
+import { getQueryCient, graphqlFetcher, QueryKeys } from "../../queryClient";
 
-import { CartType } from '../../graphql/cart'
+const CartItem = ({ id, imageUrl, price, title, amount }: CartType) => {
+  const queryClient = useQueryClient();
+  const { mutate: updateCart } = useMutation(
+    ({ id, amount }: { id: string; amount: number }) =>
+      graphqlFetcher(UPDATE_CART, { id, amount }),
+    {
+      // optimistic ui
+      onMutate: async ({ id, amount }) => {
+        await queryClient.cancelQueries(QueryKeys.CART);
+        const prevCart = queryClient.getQueryData<{ [key: string]: CartType }>(
+          QueryKeys.CART
+        );
 
-const CartItem = ({ id, imageUrl, price, title, amount }: CartType) => (
-  <li>
-    {id} {imageUrl} {price} {title} {amount}
-  </li>
-)
+        if (!prevCart?.[id]) return prevCart;
+        const newCart = {
+          ...(prevCart || {}),
+          [id]: { ...prevCart[id], amount },
+        };
+        queryClient.setQueryData(QueryKeys.CART, newCart);
+        return prevCart;
+      },
+      onSuccess: (newValue) => {
+        const prevCart = queryClient.getQueryData<{ [key: string]: CartType }>(
+          QueryKeys.CART
+        );
+        const newCart = {
+          ...(prevCart || {}),
+          [id]: newValue,
+        };
+        queryClient.setQueryData(QueryKeys.CART, newCart);
+      },
+    }
+  );
+  const { mutate: deleteCart } = useMutation(
+    ({ id }: { id: string }) => graphqlFetcher(DELETE_CART, { id }),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(QueryKeys.CART);
+      },
+    }
+  );
 
-export default CartItem
+  const handleUpdateAmount = useCallback(
+    (e: SyntheticEvent) => {
+      const amount = Number((e.target as HTMLInputElement).value);
+      if (amount < 1) return;
+      updateCart({ id, amount });
+    },
+    [updateCart, id, amount]
+  );
+
+  const handleDeleteItem = useCallback(() => {
+    deleteCart({ id });
+  }, [deleteCart, id]);
+  return (
+    <li className="cart-item">
+      <input
+        className="cart-item__checkbox"
+        type="checkbox"
+        name="select-item"
+      />
+      <img className="cart-item__image" src={imageUrl} alt={`cart_img_${id}`} />
+      <p className="cart-item__price">{price}</p>
+      <p className="cart-item__title">{title}</p>
+      <input
+        className="cart-item__amount"
+        type="number"
+        value={amount}
+        min={1}
+        onChange={handleUpdateAmount}
+      />
+      <button
+        className="cart-item__button"
+        type="button"
+        onClick={handleDeleteItem}
+      >
+        삭제
+      </button>
+    </li>
+  );
+};
+
+export default CartItem;
