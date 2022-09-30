3845deec01eafb800275b77c4cf7ae1ab71b3507
HomieKim <ghals0708@gmail.com>
2022-09-30 23:56:18 +0900

    user hook 수정

--- context-api-tutorial/src/components/user-list.tsx
+++ context-api-tutorial/src/components/user-list.tsx
@@ -1,24 +1,9 @@
 import React from "react";
-import { getUser } from "../apis/user";
 import useUsers from "../hooks/use-users";
-import { useUserDispatch, useUsersState } from "../utils/custom-context";
 
 const UserList = () => {
-  const { isLoading, data, error } = useUsers();
-  const { user } = useUsersState();
-  const dispatch = useUserDispatch();
-  console.log(data);
-
-  const selectHandler = async (id: number) => {
-    if(user.data !== null && user.data.id === id) return;
-    dispatch({ type: "GET_USER" });
-    try {
-      const response = await getUser(id);
-      dispatch({ type: "GET_USER_SUCCESS", data: response });
-    } catch (e) {
-      dispatch({ type: "GET_USER_FAILURE", error: e as unknown as Error });
-    }
-  };
+  const { isLoading, data, error, selectHandler } = useUsers();
+ 
   if (isLoading) return <div>loading ... </div>;
   if (error) return <div>error!!</div>;
   return (
--- context-api-tutorial/src/hooks/use-users.ts
+++ context-api-tutorial/src/hooks/use-users.ts
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { getUsers } from "../apis/user";
+import { getUser, getUsers } from "../apis/user";
 import { User } from "../typings/user";
 import { useUserDispatch, useUsersState } from "../utils/custom-context";
 
@@ -7,9 +7,10 @@ type ReturnType = {
   isLoading: boolean;
   data: Array<User> | null;
   error: Error | null;
+  selectHandler : (id:number) => void;
 };
 const useUsers = (): ReturnType => {
-  const { users } = useUsersState();
+  const { users, user } = useUsersState();
   const dispatch = useUserDispatch();
 
   useEffect(() => {
@@ -25,10 +26,22 @@ const useUsers = (): ReturnType => {
     if (users.data === null) fetchUsers();
   }, [dispatch, users.data]);
 
+  const selectHandler = async (id: number) => {
+    if (user.data !== null && user.data.id === id) return;
+    dispatch({ type: "GET_USER" });
+    try {
+      const response = await getUser(id);
+      dispatch({ type: "GET_USER_SUCCESS", data: response });
+    } catch (e) {
+      dispatch({ type: "GET_USER_FAILURE", error: e as unknown as Error });
+    }
+  };
+
   return {
     isLoading: false,
     data: users.data,
     error: users.error,
+    selectHandler: selectHandler,
   };
 };
 
